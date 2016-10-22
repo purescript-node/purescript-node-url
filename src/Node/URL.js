e27c9d986b9bd22ac89e5acbd92b1f7d26c99162
Gary Burgess <gary.burgess@gmail.com>
2016-10-22 14:22:45 +0100

    Prepare for 2.0 release

--- src/Node/URL.js
+++ src/Node/URL.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var url = require('url');
-var queryString = require('querystring');
+var url = require("url");
+var queryString = require("querystring");
 
 exports.parse = url.parse;
 
 exports.format = url.format;
 
-exports.resolve = function(from) {
-    return function(to) {
-        return url.resolve(from, to);
-    }
+exports.resolve = function (from) {
+  return function (to) {
+    return url.resolve(from, to);
+  };
 };
 
 exports.parseQueryString = queryString.parse;
