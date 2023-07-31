2fbb790c4fcb00a7bb7942a751048f089a486b7f
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-31 17:37:18 -0500

    Update tests

--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,12 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Node.URL (format, parse, parseQueryString, toQueryString)
+import Node.URL as URL
 import Test.Assert (assertEqual)
 
 main ∷ Effect Unit
 main = do
-  assertEqual { expected: "http://example.com/", actual: format $ parse "http://example.com/" }
-  assertEqual { expected: "foo=42", actual: toQueryString $ parseQueryString "foo=42" }
+  let urlStr = "http://example.com/"
+  url <- URL.new urlStr
+  urlStr' <- URL.format url
+  assertEqual { expected: urlStr, actual: urlStr' }
